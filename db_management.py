a5a3b5ee157fb16242fe8b2a061acddb1f2dff9f
Tali <tali693@gmail.com>
2018-11-28 11:42:10 +0200

    implementation db_management functions

--- db_management.py
+++ db_management.py
@@ -1,5 +1,4 @@
 import settings
-import secret_settings
 import pymongo.mongo_client
 
 
@@ -10,11 +9,11 @@ class DBManagement:
         self.db = self.client.get_database(settings.db)
         self.collection = self.db.get_collection(settings.collection)
 
-    def init_user(self, chat_id, status):
+    def insert_user(self, chat_id, status):
         pass
 
-    def update_status(self, chat_id):
-        pass
+    def update_status(self, chat_id, status):
+        self.collection.update_one({'chat_id': chat_id, }, {'status': status, })
 
     def insert_movie(self, chat_id):
         pass
@@ -25,8 +24,13 @@ class DBManagement:
     def delete_all_movies(self, chat_id):
         pass
 
-    def insert_chosen_movie(self, chat_id):
-        pass
+    def insert_chosen_movie(self, chat_id, movie):
+        self.collection.update_one({'chat_id': chat_id, },
+                                   {"$set": {"movie": {'name': movie['name'],
+                                                       'place': movie['place'],
+                                                       'time': movie['time'],
+                                                       'trailer': movie['trailer'],
+                                                       'image': movie['image']}}})
 
     def get_all_movies(self, chat_id):
         pass
@@ -37,8 +41,10 @@ class DBManagement:
     def get_date(self, chat_id):
         pass
 
-    def insert_date(self, chat_id, is_notification=True):
-        pass
+    def insert_date(self, chat_id, date, is_notification=True):
+        self.collection.update_one({'chat_id': chat_id, },
+                                   {'date': date,
+                                    'is_notification': is_notification})
 
 
 
