6f8700e872ba0de48850f10ebcfbffafdbdead27
chani frenkel <632chani@gmail.com>
2018-11-28 11:53:10 +0200

    part of implementation

--- db_management.py
+++ db_management.py
@@ -8,38 +8,42 @@ class DBManagement:
         self.db = self.client.get_database(settings.db)
         self.collection = self.db.get_collection(settings.collection)
 
-    def init_user(self, chat_id, status):
-        pass
+    def insert_user(self, chat_id, status):
+        self.collection.insert_one({"chat_id":chat_id},{"status":status,"movies_list":[], "date":None, "chosen_movie":None, "is_notification":None})
 
-    def update_status(self, chat_id):
-        pass
+    def update_status(self, chat_id,status):
+        self.collection.update_one({"chat_id":chat_id},{"$set":{"status":status}})
 
-    def insert_movie(self, chat_id):
-        pass
+    def insert_movie(self, chat_id, movie):
+        movie_list = self.collection.find_one({},{"chat_id":chat_id})["movies_list"]
+        movie_list.append(movie)
+        self.collection.update_one({"chat_id": chat_id}, {"$set": {"movies_list": movie_list}})
 
     def delete_movie(self, chat_id):
         pass
 
     def delete_all_movies(self, chat_id):
-        pass
+        self.collection.update_one({"chat_id": chat_id}, {"$set": {"movies_list": []}})
 
     def insert_chosen_movie(self, chat_id):
         pass
 
     def get_all_movies(self, chat_id):
-        pass
+        return self.collection.find_one({}, {"chat_id": chat_id})["movies_list"]
 
     def get_chosen_movie(self, chat_id):
-        pass
+        return self.collection.find_one({}, {"chat_id": chat_id})["chosen_movie"]
 
     def get_date(self, chat_id):
-        pass
+        return self.collection.find_one({},{"chat_id":chat_id})["date"]
 
-    def insert_date(self, chat_id, is_notification=True):
+    def insert_date(self, chat_id, date, is_notification=True):
         pass
 
     def find_user(self, chat_id):
-        pass
+        if self.collection.find({},{"chat_id":chat_id}):
+            return True
+        return False
 
 
 
